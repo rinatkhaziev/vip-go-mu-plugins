7594bb1329677f2a0765bc6bb9d60119d9ad762e
Mohammad Jangda <batmoo@gmail.com>
2018-04-19 01:00:54 -0400

    Respect multisite's space limits
    If a multisite network limits the file upload size, we should respect
    that value instead of forcing it to our global maximum.

--- a8c-files.php
+++ a8c-files.php
@@ -24,9 +24,16 @@ class A8C_Files {
 	function __construct() {
 
 		// Upload size limit is 1GB
-		add_filter( 'upload_size_limit', function() {
-			return GB_IN_BYTES;
-		} );
+		add_filter( 'upload_size_limit', function( $limit ) {
+			$max_limit = GB_IN_BYTES;
+
+			$upload_space_check_disabled = get_site_option( 'upload_space_check_disabled' );
+			if ( ! $upload_space_check_disabled ) {
+				return min( $current_limit, $max_limit );
+			}
+
+			return $max_limit;
+		}, 9999 ); // Run much later than core's callback since we want to override it
 
 		// Hooks for the mu-plugin WordPress Importer
 		add_filter( 'load-importer-wordpress', array( &$this, 'check_to_download_file' ), 10 );
